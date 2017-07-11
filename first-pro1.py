3526c6c0544b325c9da4dc711c0423d3aa1ca274
John Doe <jdoe@domain.com>
2017-07-11 14:30:32 -0700

    first-pro1.py

---
+++ first-pro1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+ip_add1 = "192.168.1.1"
+ip_add2 = "192.168.2.1"
+ip_add3 = "192.168.3.1"
+
+ip_add4 =  raw_input ("please enter ip")
+
+
+print("{:>30}".format(ip_add1))
+print("{:>30}".format(ip_add2))
+print("{:>30}".format(ip_add3))
+
+
